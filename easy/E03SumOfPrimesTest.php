828c131239518ca44a513dfa26cc947f589e5885
Zarsova <shrsakai@gmail.com>
2015-02-16 02:32:14 +0900

    Add E04PrimePalindrome

--- easy/E03SumOfPrimesTest.php
+++ easy/E03SumOfPrimesTest.php
@@ -7,10 +7,10 @@ class E03SumOfPrimesTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->logic = new E03SumOfPrimes;
+        $this->logic = new E03SumOfPrimes();
     }
 
-    public function testSolveFizzBuzz()
+    public function testSolve()
     {
         $this->assertEquals(3682913,
             $this->logic->sumOfPrimes(1000));
